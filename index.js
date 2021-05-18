1c36f586d0dc91259bed0c5d1ae6baa981e08ea5
Carlos Brito <cbrito1994@gmail.com>
2021-05-18 18:15:51 -0500

    Added some of the questions to use in inquirer

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'Enter the team manager’s name',
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: 'Enter the team manager’s employee ID',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the team manager’s email address',
+    },
+    {
+        type: 'input',
+        name: 'number',
+        message: 'Enter the team manager’s office number',
+    },
+    {
+        type: 'checkbox',
+        message: 'Please add an engineer or an intern or choose to finish building my team',
+        name: 'license',
+        choices: ['Engineer', 'Intern', 'Finish up'],
+    }
+];
