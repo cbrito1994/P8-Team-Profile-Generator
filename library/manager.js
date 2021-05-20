6882c476956df27f081a2fd1b82824fb5a3168f0
Carlos Brito <cbrito1994@gmail.com>
2021-05-20 13:55:24 -0500

    Writing the functions with inquirer for manager.js file

--- library/manager.js
+++ library/manager.js
@@ -1,9 +1,19 @@
+const inquirer = require('inquirer');
 const Employee = require('./employee');
 
 class Manager extends Employee{
     constructor(officeNumber){
         super();
         this.officeNumber = officeNumber;
+    }
+    getOfficeNumber(){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Enter the team manager’s office number',
+            }
+        ])
     }
     getRole(){
         return 'Manager'
