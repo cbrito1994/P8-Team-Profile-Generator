c355b0b8e834fcd309d84e6a677f5b9e32ea4861
Carlos Brito <cbrito1994@gmail.com>
2021-05-23 21:49:37 -0500

    Corrected the manager class

--- library/manager.js
+++ library/manager.js
@@ -1,22 +1,15 @@
-const inquirer = require('inquirer');
 const Employee = require('./employee');
 
 class Manager extends Employee{
-    constructor(officeNumber){
-        super();
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
         this.officeNumber = officeNumber;
     }
     getOfficeNumber(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'officeNumber',
-                message: 'Enter the team manager’s office number',
-            }
-        ])
+        return this.officeNumber;
     }
     getRole(){
-        return 'Manager'
+        return 'Manager';
     }
 }
 
