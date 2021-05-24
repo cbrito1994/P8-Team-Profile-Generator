3e8f43e1dbf857e24c075a5a36889e3bbfd44dd4
Carlos Brito <cbrito1994@gmail.com>
2021-05-23 21:47:25 -0500

    Corrected the employee class

--- library/employee.js
+++ library/employee.js
@@ -1,41 +1,21 @@
-const inquirer = require("inquirer");
-
 class Employee {
-    constructor(name, id, email){
+    constructor(name, id, email) {
         this.name = name;
         this.id = id;
         this.email = email;
     }
     getName(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Enter the team manager’s name',
-            }
-        ])
-    }
+        return this.name;
+    };
     getId(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'id',
-                message: 'Enter the team manager’s employee ID',
-            }
-        ])
-    }
+        return this.id;
+    };
     getEmail(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Enter the team manager’s email address',
-            }
-        ])
-    }
+        return this.email;
+    };
     getRole(){
-        return 'Employee'
-    }
+        return 'Employee';
+    };
 }
 
-module.exports = Employee
+module.exports = Employee;
