caa2a7da9efc75f11a4a9670693faf4e8802cac2
Carlos Brito <cbrito1994@gmail.com>
2021-08-02 20:41:16 -0500

    Corrected typos

--- src/htmlTemplate.js
+++ src/htmlTemplate.js
@@ -3,12 +3,12 @@ const generateTeam = team => {
     // create the manager html
     const managerHtml = manager => {
         return `
-            <div class="card employee-card">
+            <div class="card employee-container">
                 <div class="card-header">
                     <h2 class="card-title">${manager.getName()}</h2>
                     <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
                 </div>
-                <div class="card-body">
+                <div class="card-container">
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${manager.getId()}</li>
                         <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
@@ -22,12 +22,12 @@ const generateTeam = team => {
     // create the html for engineers
     const engineerHtml = engineer => {
         return `
-            <div class="card employee-card">
+            <div class="card employee-container">
                 <div class="card-header">
                     <h2 class="card-title">${engineer.getName()}</h2>
                     <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
                 </div>
-                <div class="card-body">
+                <div class="card-container">
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${engineer.getId()}</li>
                         <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
@@ -41,12 +41,12 @@ const generateTeam = team => {
     // create the html for interns
     const internHtml = intern => {
         return `
-            <div class="card employee-card">
+            <div class="card employee-container">
                 <div class="card-header">
                     <h2 class="card-title">${intern.getName()}</h2>
                     <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
                 </div>
-                <div class="card-body">
+                <div class="card-container">
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${intern.getId()}</li>
                         <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
@@ -94,14 +94,14 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-header">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+            <div class="team-container col-12 d-flex justify-content-center">
                 ${generateTeam(team)}
             </div>
         </div>
